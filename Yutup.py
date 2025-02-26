c0a35d379bd22bf1a80b543f6370e53308d22e7a
alpha-Cw <avisiniapus@gmail.com>
2025-02-26 07:30:00 +0700

    Update Yutup.py

--- Yutup.py
+++ Yutup.py
@@ -18,7 +18,6 @@ print("")
 if not data:
   print("tolong masukan link/urlnya!")
 else:
-  print("")
   subprocess.run(["yt-dlp", "-x", "--audio-format", "mp3", "-o", "hasilDownload/%(title)s.%(ext)s", data])
   print("")
   print("PERINTAH SELESAI")
